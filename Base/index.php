06b44d95ef362ad7194a3fa5ad3df900b401d17f
Gabriel Dragoni <ga.dragoni@gmail.com>
2018-05-22 13:11:25 -0300

    Commit Dragoni
    Adicionado arquivo de estilos
    Alterado nome da página e ícone do navegador

--- Base/index.php
+++ Base/index.php
@@ -6,15 +6,16 @@
 ?>
 <!DOCTYPE html>
 <html lang="pt_BR">
-
+<title>
+    Quero ver!
+</title>
 <head>
-    <meta charset="utf-8">
+<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <link rel="shortcut icon" href="./images/facao.png" >
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="./styles.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-
 </head>
 
 <body>
