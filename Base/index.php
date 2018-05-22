45214a53e92ef949bb1429660eb5c88b0d079fda
Gabriel Dragoni <ga.dragoni@gmail.com>
2018-05-22 15:05:44 -0300

    Adicionado template inicial
    Chupa japoneis

--- Base/index.php
+++ Base/index.php
@@ -25,11 +25,105 @@
             <img class="figure-img img-fluid rounded" src="https://picsum.photos/4016/800?image=901" alt="logotipo">
         </figure>
     </header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Quero Ver!</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="add-meme.php">Adicionar Meme</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">About</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
-    <?php
-        $array = ["fsdfsd", "gsgsdfg", "gsgsdgs"];
-        foreach()
-    ?>
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+          <h1 class="my-4">Page Heading
+            <small>Secondary Text</small>
+          </h1>
+        
+        <?php
+            $lista = ["fsdfsd", "gsgsdfg", "gsgsdgs"];
+            foreach($lista as $index => $item){
+                echo str_replace("[CONTENT]", $item, "
+                <!-- Blog Post -->
+                <div class='card mb-4'>
+                  <img class='card-img-top' src='http://placehold.it/750x300' alt='Card image cap'>
+                  <div class='card-body'>
+                    <h2 class='card-title'>Post Title</h2>
+                    <p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+                    <a href='#' class='btn btn-primary'>Read More &rarr;</a>
+                  </div>
+                  <div class='card-footer text-muted'>
+                    Posted on January 1, 2017 by
+                    <a href='#'>Start Bootstrap</a>
+                  </div>
+                </div>");
+            }
+        ?>
+
+          <!-- Pagination -->
+          <ul class="pagination justify-content-center mb-4">
+            <li class="page-item">
+              <a class="page-link" href="#">&larr; Older</a>
+            </li>
+            <li class="page-item disabled">
+              <a class="page-link" href="#">Newer &rarr;</a>
+            </li>
+          </ul>
+
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+        <div class="col-md-4">
+
+          <!-- Search Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Search</h5>
+            <div class="card-body">
+              <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search for...">
+                <span class="input-group-btn">
+                  <button class="btn btn-secondary" type="button">Go!</button>
+                </span>
+              </div>
+            </div>
+          </div>    
+
+          <!-- Side Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Side Widget</h5>
+            <div class="card-body">
+              You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
     
     <footer data-spy="scroll" data-target="#menu_navbar" data-offset="0">
         <p id="rodape" style="text-align: center;">Criado por mim</p>
